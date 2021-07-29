54f9aa9010e38bba56991dcc9a04ff34537fb17f
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-07-29 21:08:40 +0200

    delete all file before delvering to instructor

--- deletePost.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //connection
- require_once("classes/db.php");
- $conn= new db();
- $conn->deleteData("posts","Post_Id={$_GET['id']}");
-
-
- header("Location:index.php");
- ?>
--- viewprofile.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$con = new PDO("mysql:host=localhost; dbname=Facebook","root","");
-$data=$con->query("select * from users where User_Id={$_COOKIE['userid']}");
-$profile=$data->fetch(PDO::FETCH_ASSOC);
-?>
